50947fcfa7fe7254d163723a3fdde20f6ed85b99
Jorge Antonio Diaz-Benito Soriano <jorg3mnf23@gmail.com>
2014-08-04 14:26:06 +0200

    Added MainActivity.

---
+++ app/src/main/java/org/jorge/lbudget/ui/activities/MainActivity.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of LBudget.
+ * LBudget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * LBudget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with LBudget. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jorge.lbudget.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.jorge.lbudget.R;
+import org.jorge.lbudget.ui.frags.NavigationToolbarFragment;
+
+public class MainActivity extends Activity implements NavigationToolbarFragment.NavigationToolbarListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMenuSelected() {
+        //TODO
+    }
+}
