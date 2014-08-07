62d831979d97538c95f2184202123cebf973368b
Jorge Antonio Diaz-Benito Soriano <jorg3mnf23@gmail.com>
2014-08-07 13:54:39 +0200

    Added backup request method

--- app/src/main/java/org/jorge/lbudget/logic/LBackupAgent.java
+++ app/src/main/java/org/jorge/lbudget/logic/LBackupAgent.java
@@ -15,6 +15,29 @@
 package org.jorge.lbudget.logic;
 
 import android.app.backup.BackupAgentHelper;
+import android.app.backup.BackupManager;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.Context;
 
-public class LBackupAgent extends BackupAgentHelper {
+import org.jorge.lbudget.utils.LBudgetUtils;
+
+public abstract class LBackupAgent extends BackupAgentHelper {
+
+    private static final String PREFERENCES_BACKUP_KEY = "PREFERENCES_BACKUP_KEY";
+
+    @Override
+    public void onCreate() {
+        Context appContext = getApplicationContext();
+
+        String[] allPreferences = LBudgetUtils.getStringArray(appContext, "all_preferences");
+        SharedPreferencesBackupHelper sharedPreferencesBackupHelper = new SharedPreferencesBackupHelper(appContext, allPreferences);
+        addHelper(PREFERENCES_BACKUP_KEY, sharedPreferencesBackupHelper);
+
+        //TODO Add the database and the pictures
+    }
+
+    public static void requestBackup(Context appContext) {
+        BackupManager bm = new BackupManager(appContext);
+        bm.dataChanged();
+    }
 }
