81c01db38699b2339503be1a1e8ba1d8aa9793d1
Jorge Antonio Diaz-Benito Soriano <jorg3mnf23@gmail.com>
2014-08-24 11:50:00 +0200

    Refactoring

--- app/src/main/java/org/jorge/lbudget/ui/frags/MovementListFragment.java
+++ app/src/main/java/org/jorge/lbudget/ui/frags/MovementListFragment.java
@@ -43,14 +43,14 @@ public class MovementListFragment extends Fragment implements UndoBarShowStateLi
         mMovementsView.setItemAnimator(new DefaultItemAnimator());
         final MovementListRecyclerAdapter mAdapter;
         mMovementsView.setAdapter(mAdapter = new MovementListRecyclerAdapter(view.findViewById(android.R.id.empty), this, mMovementsView, getActivity(), MovementManager.getInstance().getSelectedAccountMovementsToDate()));
-        mNewMovementButton = (FloatingActionButton) view.findViewById(R.id.button_new_account);
-        mNewMovementButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAdapter.createNewMovement();
-            }
-        });
-        mNewMovementButton.attachToRecyclerView(mMovementsView);
+//        mNewMovementButton = (FloatingActionButton) view.findViewById(R.id.button_new_account); TODO Uncomment when the button is added
+//        mNewMovementButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mAdapter.createNewMovement(); TODO Uncomment when the button is added
+//            }
+//        });
+//        mNewMovementButton.attachToRecyclerView(mMovementsView); TODO Uncomment when the button is added
     }
 
     @Override
@@ -77,11 +77,11 @@ public class MovementListFragment extends Fragment implements UndoBarShowStateLi
 
     @Override
     public void onShowUndoBar() {
-        mNewMovementButton.setEnabled(Boolean.FALSE);
+//        mNewMovementButton.setEnabled(Boolean.FALSE); TODO Uncomment when the button is added
     }
 
     @Override
     public void onHideUndoBar() {
-        mNewMovementButton.setEnabled(Boolean.TRUE);
+//        mNewMovementButton.setEnabled(Boolean.TRUE); TODO Uncomment when the button is added
     }
 }
